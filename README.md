20b658a595618aad2b038cfdce7162abda9707e9
Francesco Bailo <me@francescobailo.net>
2018-07-30 10:47:04 +1000

    Init

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Opinion-polling-for-the-next-Italian-general-election
+
+Parser for data collected from Wikipedia. The dataset is stored on [Dataverse](https://doi.org/10.7910/DVN/IFSWXO). To cite:
+
+```bib
+@data{DVN/IFSWXO_2018,
+author = {Bailo, Francesco},
+publisher = {Harvard Dataverse},
+title = {Opinion polling for the next Italian general election | wikidata.org/wiki/Q54437087},
+year = {2018},
+doi = {10.7910/DVN/IFSWXO},
+url = {https://doi.org/10.7910/DVN/IFSWXO}
+}
+```
+
+## Source
+
+Wikipedia opinion polling data are available on these pages:
+
+* [Opinion_polling_for_the_next_Italian_general_election](https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Italian_general_election)
+* [Opinion_polling_for_the_Italian_general_election,_2013](https://en.wikipedia.org/wiki/Opinion_polling_for_the_Italian_general_election,_2013)
+
+
+## Code
+
+The R script `read_raw_xlsx.R` takes care of parsing the information from the different sheets of the Excel spreadsheet into a long-format data frame. It also adds Wikidata identifiers (when available). 
+
+## Data
+
+Observations in the dataset `open_csv_italian_party_polls.csv`  are defined for this variables
+* `polling_firm`: The name of the polling firm.
+* `variable`: The name of the variable. It can be a party label to indicate , `Sample size` or `Other`. 
+* `value`: Double value of the observation.
+* `date`: The last date in which of the survey was administered. 
+* `wikidata_id`: The Wikidata identifier for the party in the format `Q\d+(\+Q\d+)?`.  A `+` between two identifiers indicates that entity is a "join" of two Wikidata entities while `-` indicates that that the entity refers to two different Wikidata entities conditionally on the date. 
+
+
---
+++ read_raw_xlsx.R
@@ -0,0 +1,64 @@
+formatDate <- function(x, year) {
+  
+  x <- gsub("31 Nov", "30 Nov", x)
+  
+  if (grepl("\\d{5}", x)) {
+    return(as.character(as.Date("1900-01-01") + as.numeric(x) - 2)) # This is how Excel stores numbers
+  } else if (grepl("[A-Za-z]{3}(-|–)\\d{1,2}", x)) {
+    return(as.character(as.Date(paste0(gsub("^(.*)(-|–)", "", x), " ", year), "%d %b %Y")))
+  } else {
+    return(as.character(as.Date(paste0(gsub("^\\d+(-|–)", "", x), " ", year), "%d %b %Y")))
+  }
+}
+
+excel_file <- "~/public_git/ScrapeOpen/Opinion-polling-for-the-next-Italian-general-election/wikipedia_data.xlsx"
+
+library(readxl)
+sheets <- excel_sheets(excel_file)
+
+wikipedia_data <- data.frame()
+
+library(reshape2)
+library(stringr)
+for (s in sheets) {
+  this_dat <- read_excel(excel_file, sheet = s)
+  this_dat <- this_dat[,-c(1:2)]
+  this_dat <- reshape2::melt(this_dat, id.vars = c("Date.3", "Polling firm"))
+  this_dat$sheet <- s
+  this_dat$date.class <- as.Date(sapply(this_dat$Date.3, formatDate, year = str_extract(s, "\\d{4}")))
+  wikipedia_data <- rbind(wikipedia_data, this_dat)
+}
+
+wikipedia_data <- subset(wikipedia_data, !variable %in% c("Lead", "Date text", "Others", "Oth."))
+wikipedia_data$value <- as.numeric(wikipedia_data$value)
+wikipedia_data <- subset(wikipedia_data, !is.na(value))
+wikipedia_data$variable <- droplevels(wikipedia_data$variable)
+
+wikipedia_data_elections <- 
+  subset(wikipedia_data, `Polling firm` %in% c('General Election','EP Election'))
+wikipedia_data <-
+  subset(wikipedia_data, !`Polling firm` %in% c('General Election','EP Election'))
+
+wikipedia_data$Date.3 <- NULL
+wikipedia_data$sheet <- NULL
+
+## `+` indicates that entity is a `join` of two Wikidata entities, 
+## `-` conditionally on date, entity refers to two different 
+##  Wikidata entities
+
+wikidata_id <- 
+  c('Q47720' = 'PdL', 'Q47729' = 'PD', 'Q47750' = 'LN', 'Q47781' = 'UdC', 'Q47768' = 'IdV', 
+    'Q286140' = 'SEL', 'Q47817' = 'M5S', 'Q47795' = 'FLI', 'None1' = 'VTR', 
+    'Q1757843' = 'FdI', 'Q1952211' = 'CD', 'Q2152252' = 'RC', 'Q2792033' = 'SC',  'Q47720+Q14924303' = 'PdL/FI', 
+    'Q15196039' = 'NCD', 'Q215350-Q14924303' = 'FI', 'SEL/SI', 'Q286140+Q21405813' = 'SI', 
+    'Q28962342' = 'AP', 'Q28841838' = 'MDP',  'Q28946245' = 'CP',  'Q44929224' = 'LeU', 
+    'Q46624077' = 'I',  'Q47389793' = 'LCP',  'Q47090559' = 'E+', 'Q46997473' = 'NcI', 'Q46217506' = 'PaP', 
+    'Q25648673' = 'CPI')
+
+wikipedia_data$wikidata_id <- names(wikidata_id)[match(wikipedia_data$variable, wikidata_id)]
+wikipedia_data$wikidata_id[wikipedia_data$wikidata_id == ""] <- NA
+wikipedia_data$wikidata_id[grepl("None", wikipedia_data$wikidata_id)] <- NA
+
+names(wikipedia_data) <- c("polling_firm", "variable", "value", "date", 'wikidata_id')
+
+write.csv(wikipedia_data, file = 'open_csv_italian_party_polls.csv', row.names = F)
